79eec13abcd3c8c72c2698a338cfed11c4f19f94
haphong463 <haphong463@gmail.com>
2024-09-13 19:30:08 +0700

    ignore jpa auto-configuration

--- api-gateway/src/main/java/net/javaguides/api_gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/net/javaguides/api_gateway/ApiGatewayApplication.java
@@ -2,8 +2,10 @@ package net.javaguides.api_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
